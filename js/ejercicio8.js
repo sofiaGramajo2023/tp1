71509dc0d397e70bb6adf42b12f25dd781113152
Sofia <sofia.asgp@gmail.com>
2023-02-28 23:03:25 -0300

    desarrollo de ejercicio 8

---
+++ js/ejercicio8.js
@@ -0,0 +1,10 @@
+
+
+numero = window.prompt('Ingrese un número','Escriba aquí el primer número');
+
+if( numero % 2 ===0){
+    document.write ('El número ingresado si es divisible por  2');
+}
+else {
+    document.write ('El número ingresado NO es divisible por  2');
+}

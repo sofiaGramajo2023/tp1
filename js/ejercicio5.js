2075bdf8d5953cb88a50d39cf6693eae75a770a2
Sofia <sofia.asgp@gmail.com>
2023-02-28 22:18:28 -0300

    desarrollo del ejercicio 5

---
+++ js/ejercicio5.js
@@ -0,0 +1,8 @@
+
+numero1 = window.prompt('Ingrese un número','Escriba aquí el primer número');
+
+numero2 = window.prompt('Ingrese un número','Escriba aquí el segundo número');
+
+
+
+document.write(parseInt (numero1)+ parseInt(numero2));

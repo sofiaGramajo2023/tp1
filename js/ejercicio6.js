0efbedcd1afe3f3a1e465bb35b5f6f14898a8b09
Sofia <sofia.asgp@gmail.com>
2023-02-28 22:38:33 -0300

    desarrollo del ejercicio 6

---
+++ js/ejercicio6.js
@@ -0,0 +1,13 @@
+
+numero1 = window.prompt('Ingrese un número','Escriba aquí el primer número');
+
+numero2 = window.prompt('Ingrese un número','Escriba aquí el segundo número');
+
+
+if ( numero1 > numero2 ){
+    document.write ('El número' + ' ' + numero1 + ' ' + 'es mayor que' + ' ' + numero2);
+} 
+else{
+    document.write ('El número' + ' ' + numero2 + ' ' + 'es mayor que' + ' ' + numero1);
+
+}
